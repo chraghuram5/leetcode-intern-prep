74ef2c4f39f868c0017c52d49ba9edf46b4c2645
chraghuram5 <ch.raghuram5@gmail.com>
2022-01-04 16:17:32 -0600

    Time: 5 ms (11.10%), Space: 62.3 MB (9.56%) - LeetHub

--- 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.java
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.java
@@ -1,17 +1,21 @@
 class Solution {
     public int maxScore(int[] cardPoints, int k) {
-        int[] prefix = new int[k+1];
-        int[] suffix = new int[k+1];
-        prefix[0]=0;
-        for(int i=1;i<=k;i++)
-            prefix[i]=prefix[i-1]+cardPoints[i-1];
-        suffix[k]=0;
-        int l=cardPoints.length-1;
-        for(int j=k-1;j>=0;j--)
-            suffix[j]=suffix[j+1]+cardPoints[l--];
-        int sum=Integer.MIN_VALUE;
-        for(int i=0;i<=k;i++)
-            sum=Math.max(prefix[i]+suffix[i], sum);
-        return sum;
+        int minSum=Integer.MAX_VALUE;
+        int tempSum=0;
+        for(int i=0;i<cardPoints.length-k;i++)
+            tempSum=tempSum+cardPoints[i];
+        minSum=Math.min(tempSum, minSum);
+        int i=0;
+        int j=cardPoints.length-k;
+        while(i<=j && j<=cardPoints.length-1){
+            tempSum=tempSum-cardPoints[i]+cardPoints[j];
+            i++;
+            j++;
+            minSum=Math.min(tempSum, minSum);
+        }
+        int sum=0;
+        for(int m=0;m<cardPoints.length;m++)
+            sum=sum+cardPoints[m];
+        return sum-minSum;
     }
 }
