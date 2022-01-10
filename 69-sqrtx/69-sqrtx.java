b3fffd9132fd1972666691ff19b287d1aa56281b
chraghuram5 <ch.raghuram5@gmail.com>
2022-01-09 23:30:33 -0600

    Time: 3 ms (27.30%), Space: 38.1 MB (21.22%) - LeetHub

---
+++ 69-sqrtx/69-sqrtx.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int mySqrt(int x) {
+        int A=x;
+        long low=0L;
+        long high=(long)A;
+        long answer=0L;
+        while(low<=high){
+            long mid=low+(high-low)/2;
+            if((mid*mid)<=A){
+                answer=mid;
+                low=mid+1;
+            }
+            else
+                high=mid-1;
+        }
+        return (int)answer;
+    }
+}
