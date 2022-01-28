2628a0d55a2fcd173f42dd2786d780260916e640
chraghuram5 <ch.raghuram5@gmail.com>
2022-01-28 08:30:48 -0600

    Time: 8 ms (24.91%), Space: 44.2 MB (6.83%) - LeetHub

--- 496-next-greater-element-i/496-next-greater-element-i.java
+++ 496-next-greater-element-i/496-next-greater-element-i.java
@@ -3,8 +3,10 @@ class Solution {
         Stack<Integer> s=new Stack<Integer>();
         int[] nextGreater=new int[nums2.length];
         for(int i=nums2.length-1;i>=0;i--){
+            //pop element from stack as long as it is less than the current element
             while(!s.isEmpty() && nums2[i]>=s.peek())
                 s.pop();
+            //Handle stack empty
             if(s.isEmpty())
                 nextGreater[i]=-1;
             else
@@ -12,9 +14,11 @@ class Solution {
             s.push(nums2[i]);
         }
         int[] answer=new int[nums1.length];
+        //HashMap for storing elements of nums2 for searching in an instant
         HashMap<Integer, Integer> map=new HashMap<Integer,Integer>();
         for(int i=0;i<nums2.length;i++)
             map.put(nums2[i],i);
+        //check for index of nums1 in nums2 using map and get the answer
         for(int i=0;i<nums1.length;i++){
             answer[i]=nextGreater[map.get(nums1[i])];
         }
