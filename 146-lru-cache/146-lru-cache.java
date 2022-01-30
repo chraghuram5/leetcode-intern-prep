6f587ef5c658d769c81b2a9d0c989272ebba1a04
chraghuram5 <ch.raghuram5@gmail.com>
2022-01-30 10:11:04 -0600

    Time: 69 ms (53.64%), Space: 125.9 MB (51.74%) - LeetHub

---
+++ 146-lru-cache/146-lru-cache.java
@@ -0,0 +1,81 @@
+class ListNode{
+    ListNode prev;
+    ListNode next;
+    int key;
+    int value;
+    ListNode(int key, int value){
+        this.key=key;
+        prev=null;
+        next=null;
+        this.value=value;
+    }
+}
+class LRUCache {
+    public static HashMap<Integer, ListNode> map;
+    public static ListNode head;
+    public static ListNode tail;
+    public static int capacity;
+    public static int currentSize;
+    public LRUCache(int capacity) {
+        map=new HashMap<Integer, ListNode>();
+        head=new ListNode(-1, -1);
+        tail=new ListNode(-1, -1);
+        head.next=tail;
+        tail.prev=head;
+        this.capacity=capacity;
+        currentSize=0;
+    }
+    
+    public int get(int key) {
+        if(map.get(key)==null)
+            return -1;
+        ListNode currentNode = map.get(key);
+        ListNode temp=currentNode.next;
+        temp.prev=currentNode.prev;
+        currentNode.prev.next=temp;
+        currentNode.next=head.next;
+        currentNode.next.prev=currentNode;
+        head.next=currentNode;
+        currentNode.prev=head;
+        return currentNode.value;
+    }
+    
+    public void put(int key, int value) {
+        if(map.get(key)!=null){
+            ListNode currentNode=map.get(key);
+            currentNode.value=value;
+            ListNode temp=currentNode.next;
+            temp.prev=currentNode.prev;
+            currentNode.prev.next=temp;
+            currentNode.next=head.next;
+            currentNode.next.prev=currentNode;
+            head.next=currentNode;
+            currentNode.prev=head;
+        }
+        else{
+            ListNode newNode=new ListNode(key, value);
+            if(currentSize<capacity){
+                //map.put(key, newNode);
+                currentSize++;
+            }
+            else{
+                map.remove(tail.prev.key);
+                tail.prev.prev.next=tail;
+                tail.prev=tail.prev.prev;
+            }
+            head.next.prev=newNode;
+            newNode.next=head.next;
+            newNode.prev=head;
+            head.next=newNode;
+            map.put(key, newNode);
+        }
+        
+    }
+}
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache obj = new LRUCache(capacity);
+ * int param_1 = obj.get(key);
+ * obj.put(key,value);
+ */
