0a69a90ddde7d8ea8a0f4f3867a56678f1cee157
chraghuram5 <ch.raghuram5@gmail.com>
2022-01-31 20:26:44 -0600

    Time: 30 ms (8.02%), Space: 48.5 MB (5.04%) - LeetHub

--- 36-valid-sudoku/36-valid-sudoku.java
+++ 36-valid-sudoku/36-valid-sudoku.java
@@ -43,12 +43,32 @@ class Solution {
         return true;
     }
     public boolean isValidSudoku(char[][] board) {
-        boolean rowCheck=checkRow(board);
-        boolean colCheck=checkCol(board);
-        boolean squareCheck=checkSquare(board);
+        // boolean rowCheck=checkRow(board);
+        // boolean colCheck=checkCol(board);
+        // boolean squareCheck=checkSquare(board);
         // System.out.println(rowCheck);
         // System.out.println(colCheck);
         // System.out.println(squareCheck);
-        return rowCheck&&colCheck&&squareCheck;
+        HashSet<String> set=new HashSet<String>();
+        for(int i=0;i<board.length;i++){
+            for(int j=0;j<board[0].length;j++){
+                char c=board[i][j];
+                if(c=='.')
+                    continue;
+                String row="("+i+""+")"+c;
+                String col=c+"("+j+""+")";
+                String square="("+(i/3)+""+")"+c+"("+(j/3)+""+")";
+                if(set.contains(row) || set.contains(col) || set.contains(square)){
+                    System.out.println(set);
+                    return false;
+                }
+                    //return false;
+                set.add(row);
+                set.add(col);
+                set.add(square);
+            }
+        }
+        return true;
+        //return rowCheck&&colCheck&&squareCheck;
     }
 }
