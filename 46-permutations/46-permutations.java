0271e1242d9d848a348cf1e7e084427edd1183ba
chraghuram5 <ch.raghuram5@gmail.com>
2022-02-01 07:28:59 -0600

    Time: 2 ms (68.85%), Space: 45.5 MB (5.02%) - LeetHub

--- 46-permutations/46-permutations.java
+++ 46-permutations/46-permutations.java
@@ -1,55 +1,25 @@
-import java.util.Collections;
 class Solution {
-    public static int[] nextPermutation(int[] nums){
-        int idx=-1;
-        for(int i=nums.length-2;i>=0;i--){
-            if(nums[i]<nums[i+1]){
-                idx=i;
-                break;
+    public static List<List<Integer>> helper(int[] nums, int i){
+        if(i==nums.length-1){
+            List<Integer> list=new ArrayList<Integer>();
+            list.add(nums[i]);
+            ArrayList<List<Integer>> tempList=new ArrayList<List<Integer>>();
+            tempList.add(list);
+            return tempList;
+        }
+        List<List<Integer>> nextPerm=helper(nums, i+1);
+        List<List<Integer>> newPerm=new ArrayList<List<Integer>>();
+        int size=nextPerm.get(0).size();
+        for(int j=0;j<=size;j++){
+            for(List<Integer> temp:nextPerm){
+                List<Integer> x=new ArrayList<>(temp);
+                x.add(j, nums[i]);
+                newPerm.add(x);
             }
         }
-        
-        if(idx==-1){
-            Arrays.sort(nums);
-            return nums;
-        }
-        for(int i=nums.length-1;i>=0;i--){
-            if(nums[i]>nums[idx]){
-                int temp=nums[i];
-                nums[i]=nums[idx];
-                nums[idx]=temp;
-                break;
-            }
-        }
-        Arrays.sort(nums, idx+1, nums.length);
-        return nums;
+        return newPerm;
     }
     public List<List<Integer>> permute(int[] nums) {
-        Arrays.sort(nums);
-        List<List<Integer>> list=new ArrayList<List<Integer>>();
-        int[] a=new int[nums.length];
-        for(int i=0;i<nums.length;i++)
-                a[i]=nums[i];
-        nums=nextPermutation(nums);
-        System.out.print(nums);
-        while(!Arrays.equals(nums,a)){
-            for(int i=0;i<nums.length;i++)
-                System.out.print(nums[i]+" ");
-            System.out.println();
-            Integer[] x=new Integer[nums.length];
-            for(int i=0;i<nums.length;i++)
-                x[i]=nums[i];
-            ArrayList<Integer> temp=new ArrayList<Integer>();
-            Collections.addAll(temp, x);
-            list.add(temp);
-            nums=nextPermutation(nums);
-        }
-        Integer[] x=new Integer[nums.length];
-        for(int i=0;i<nums.length;i++)
-            x[i]=nums[i];
-        ArrayList<Integer> temp=new ArrayList<Integer>();
-        Collections.addAll(temp, x);
-        list.add(temp);
-        return list;
+        return helper(nums, 0);
     }
 }
